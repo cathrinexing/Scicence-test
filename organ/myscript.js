8feece9c269cfbceda15150ad88d7765ef7c1b67
Cathrinexing <lxx101@miami.edu>
2020-04-04 11:34:06 -0400

    s

---
+++ organ/myscript.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+
+  $("#btn_brain").click(function() {
+    // $(".lung_img").hide();
+    // $(".eye_img").show();
+    // $(".lung_img").css("opacity", "0");
+    // $(".brain_img").css("opacity", "1");
+$(".layers").css("opacity", "0");
+    // $(".layers").animate({
+    //   opacity: "0"
+    // });
+    $(".layers").css("opacity", "0");
+    $(".brain_img").animate({
+      opacity: "1"
+    });
+  });
+
+  $("#btn_eyes").click(function() {
+    // $(".layers").animate({
+    //   opacity: "0"
+    // });
+        $(".layers").css("opacity", "0");
+    $(".eyes_img").animate({
+      opacity: "1"
+    });
+
+  });
+  $("#btn_nose").click(function() {
+      $(".layers").css("opacity", "0");
+    // $(".layers").animate({
+    //   opacity: "0"
+    // });
+    $(".nose_img").animate({
+      opacity: "1"
+    });
+
+  });
+  $("#btn_lungs").click(function() {
+          $(".layers").css("opacity", "0");
+    // $(".layers").animate({
+    //   opacity: "0"
+    // });
+    $(".lungs_img").animate({
+      opacity: "1"
+    });
+  });
+
+  $("#btn_heart").click(function() {
+      $(".layers").css("opacity", "0");
+    // $(".layers").animate({
+    //   opacity: "0"
+    // });
+    $(".heart_img").animate({
+      opacity: "1"
+    });
+  });
+
+  $("#btn_liver").click(function() {
+      $(".layers").css("opacity", "0");
+    // $(".layers").animate({
+    //   opacity: "0"
+    // });
+    $(".liver_img").animate({
+      opacity: "1"
+    });
+  });
+
+  $("#btn_kidneys").click(function() {
+      $(".layers").css("opacity", "0");
+    // $(".layers").animate({
+    //   opacity: "0"
+    // });
+    $(".kidneys_img").animate({
+      opacity: "1"
+    });
+  });
+
+
+
+  $("#btn_gut").click(function() {
+          $(".layers").css("opacity", "0");
+    // $(".layers").animate({
+    //   opacity: "0"
+    // });
+    $(".gut_img").animate({
+      opacity: "1"
+    });
+
+    // $(".brain_img").animate({
+    //   opacity: "0"
+    // });
+
+  });
+
+});
---
+++ trailblazing-women-scientists/js/raw/my.js
@@ -0,0 +1,9 @@
+
+
+  var myFullpage = new fullpage('#fullpage', {
+    anchors: ['firstPage', 'first2Page','secondPage', '3rdPage', '4thPage', '5thPage', '6thPage', '7thPage', '8thPage', '9thPage', '10thPage', '11thPage', '12thPage'],
+    sectionsColor: [' #000', ' #000', ' #000', ' #000', ' #000', ' #000', ' #000', ' #000', ' #000', ' #000', ' #000',' #000',' #000'],
+    navigation: true,
+    navigationPosition: 'left'
+    // navigationTooltips: ['First page', 'Second page', 'Third', 'Fourth and last page']
+  });
